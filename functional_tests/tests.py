4b75a3b84e0386c0f7eff76ae6c712b295909812
antcin <ciniglioantonio@gmail.com>
2018-08-12 20:01:01 +0100

    Start writing regression test for second user

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -63,7 +63,19 @@ class NewVisitorTest(LiveServerTestCase):
         # for her -- there is some explanatory text to that effect.
         self.fail('Finish the test!')
 
-        # She visits taht URL -- her to-do list is still there.
+        # She visits that URL -- her to-do list is still there.
 
         # Satisfied, she goes back to sleep.
+
+    def test_multiple_users_can_start_lists_at_different_urls(self):
+        # Cleopatra starts a new to-do list
+        self.browser.get(self.live_server_url)
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Buy birthday present for Marc Anthony')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: Buy birthday present for Marc Anthony')
+
+        # She notices that her list has a unique URL
+        cleopatra_list_url = self.browser.current_url
+        self.assertRegex(cleopatra_list_url, '/lists/.+')
 
